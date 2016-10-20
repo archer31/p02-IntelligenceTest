36cfced58955c03d46555e5857bc520c4e45908a
archer77 <corleyx@yahoo.com>
2016-10-19 20:14:34 -0700

    added logic
    created basic logic for chooseing which tests to give the user and how
    to know the answers to the tests

--- logic.js
+++ logic.js
@@ -1,3 +1,64 @@
 /*logic.js*/
 /*Alexander Corley*/
-  
+
+/**
+ * I use this structure in order to add more tests and choose from the pool randomly
+ * tests have the same name as their associated pages. just concat ".html" on the end to access the page
+**/
+var tests = [
+  "test1",
+  "test2",
+  "test3",
+  "test4",
+  "test5"
+];
+
+/**
+ * I dont know that the answers are yet but i will know once i actually make the tests
+ * correctAnswers[tests[0]] has the answer for tests[0] i dont know if this sort of hash table works but i think it does.
+ * since we are storing the answers as strings then we can have both string answers and number answers
+**/
+var correctAnswers = {}
+
+/**
+ * initialize the correctAnswers object with the correct key value pairs. 
+**/
+console.log("add correct answers dumbass");
+correctAnswers[tests[0]] = "";
+correctAnswers[tests[1]] = "";
+correctAnswers[tests[2]] = "";
+correctAnswers[tests[3]] = "";
+correctAnswers[tests[4]] = "";
+
+//the current test that the user is taking
+var currentTest = 0;
+
+/**
+ * Shuffles array in place.
+ * @param {Array} The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+/**
+ * choose three random tests from the list above and store them for later use
+**/
+function start() {
+//  shuffle(tests);
+  console.log("shuffle is commented");
+  window.location = tests[0].concat(".html");
+}
+
+/**
+ * check the current test for correctness
+**/
+function check() {
+  if ()
+}
