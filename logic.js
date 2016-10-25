13d9555551902cbbfcd24077cb2320082c92a2dc
archer77 <corleyx@yahoo.com>
2016-10-24 20:34:24 -0700

    generate tests
    added logic to generate tests from a set of functions and to update the
    page with the correct questions

--- logic.js
+++ logic.js
@@ -2,6 +2,18 @@
 /*Alexander Corley*/
 
 /**
+ * load all questions
+**/
+function loadQuestions() {
+  answerGenerator[tests[currentTest]]("question1");
+  answerGenerator[tests[currentTest]]("question2");
+  answerGenerator[tests[currentTest]]("question3");
+  answerGenerator[tests[currentTest]]("question4");
+  answerGenerator[tests[currentTest]]("question5");
+  answerGenerator[tests[currentTest]]("question6");
+}
+
+/**
  * I use this structure in order to add more tests and choose from the pool randomly
  * tests have the same name as their associated pages. just concat ".html" on the end to access the page
 **/
@@ -12,10 +24,67 @@ var tests = [
   "test4"
 ];
 
-//store the functions to generate answers and examples for the tests
+/**
+ * store the functions to generate answers and examples for the tests
+ * every function should have the param ID, which specifies the paragraph tag id to put the question in
+**/
 var answerGenerator = {};
 
+answerGenerator[tests[0]] = function(ID) {
+  var para = document.getElementById(ID);
+  var A, B, C;
+  A = Math.floor(Math.random()*10);
+  B = Math.floor(Math.random()*10);
+  C = toText(A).length + toText(B).length;
+  if (ID == "question6") {
+    para.innerHTML = "<span>".concat(A, ",", B, "->???</span>");
+    correctAnswers[tests[currentTest]] = C;
+  } else {
+    para.innerHTML = A.toString().concat(",", B, "->", C);
+  }
+}
 
+answerGenerator[tests[1]] = function(ID) {
+  var para = document.getElementById(ID);
+  var A, B, C;
+  A = Math.floor(Math.random()*21);
+  B = Math.floor(Math.random()*21);
+  C = (toText(A).length * A) + (toText(B).length * B);
+  if (ID == "question6") {
+    para.innerHTML = "<span>".concat(A, ",", B, "->???</span>");
+    correctAnswers[tests[currentTest]] = C;
+  } else {
+    para.innerHTML = A.toString().concat(",", B, "->", C);
+  }
+}
+
+//utility functions for generating questions and answers
+function toText(num) {
+  switch (num) {
+    case 0:   return "zero";
+    case 1:   return "one";
+    case 2:   return "two";
+    case 3:   return "three";
+    case 4:   return "four";
+    case 5:   return "five";
+    case 6:   return "six";
+    case 7:   return "seven";
+    case 8:   return "eight";
+    case 9:   return "nine";
+    case 10:  return "ten";
+    case 11:  return "eleven";
+    case 12:  return "twelve";
+    case 13:  return "thirteen";
+    case 14:  return "fourteen";
+    case 15:  return "fifteen";
+    case 16:  return "sixteen";
+    case 17:  return "seventeen";
+    case 18:  return "eighteen";
+    case 19:  return "nineteen";
+    case 20:  return "twenty";
+    default:  return "";
+  }
+}
 
 /**
  * I dont know that the answers are yet but i will know once i actually make the tests
@@ -50,7 +119,7 @@ function shuffle(a) {
 function start() {
 //  shuffle(tests);
   console.log("shuffle is commented");
-  window.location = tests[0].concat(".html");
+  window.location = "test.html";
 }
 
 /**
@@ -86,10 +155,10 @@ function check() {
 function nextPage() {
   currentTest++;
   var inputValue = document.getElementById("input").value;
-if (correctAnswers[tests[currentTest]] == inputValue) {
+  if (correctAnswers[tests[currentTest]] == inputValue) {
     points++;
   } else if (inputValue == "") {
     window.alert("you have not entered an answer yet.\nare you sure you want to move on?")
   }
-  window.location = tests[currentTest].concat(".html");
+  loadQuestions();
 }
